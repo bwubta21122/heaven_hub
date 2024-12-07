0c6096fd292ef925629574115f9efd9fd4b2cfa5
Aritra Banik <ar1trabanik@Aritras-MacBook-Air.local>
2024-12-08 03:11:49 +0530

    14

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,19 @@
-module.exports = {
+// module.exports = {
+//   content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     // require('@tailwindcss/line-clamp'),
+//     // ...
+//   ],
+// };
+
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    // require('@tailwindcss/line-clamp'),
-    // ...
-  ],
+  plugins: [],
 };
