bf4e65c49c0a5c449132e9f34d02addc6b41ba47
Aritra Banik <ar1trabanik@Aritras-MacBook-Air.local>
2024-12-08 02:55:38 +0530

    10

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,51 +1,5 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const dotenv=require("dotenv");
-const userRouter=require("./routes/userRouter");
-const authRouter=require("./routes/authRouter");
-const listingRouter=require("./routes/listingRouter");
-
-const cookieParser=require("cookie-parser");
-const path=require("path");
-
-dotenv.config();
-mongoose.connect(`${process.env.MONGO}/heaven_hub`).then(()=>{
-    console.log("connected to db");
-})
-.catch((err)=>{
-    console.log(err);
-})
-const __dirName=path.resolve();
-
-
-const app=express();
-const cors = require('cors');
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-// app.use((req, res, next) => {
-//     console.log(`Request received: ${req.method} ${req.url}`);
-//     next();
-//   });
-app.use(express.json());
-app.use(cookieParser());
+const app=require("./app");
 
 app.listen(3000,()=>{
     console.log("server is runnning in 3000");
-});
-
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
-app.use("/api/listing",listingRouter);
-
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || "Internal Server Error";
-    res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    })
 });
